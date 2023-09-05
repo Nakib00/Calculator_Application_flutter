ce9900516fa16e3669a95f3e4528e3ec8818dd6c
Nakib00 <2022142@iub.edu.bd>
2023-09-05 13:11:50 +0600

    comment add

--- lib/CalculatorApp.dart
+++ lib/CalculatorApp.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
 import 'package:math_expressions/math_expressions.dart';
-
-
 
 class CalculatorApp extends StatefulWidget {
   const CalculatorApp({super.key});
@@ -14,7 +10,6 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
-
   double firstnum = 0.0;
   double secondnum = 0.0;
   var input = '';
@@ -28,23 +23,33 @@ class _CalculatorAppState extends State<CalculatorApp> {
         output = '';
       });
     } else if (value == "<") {
-      setState(() {
+      if (input.isNotEmpty) {
         input = input.substring(0, input.length - 1);
-      });
+      }
+      setState(() {});
     } else if (value == "=") {
-      setState(() {
-        var userInput = input;
-        userInput = userInput.replaceAll("x", "*");
-        try {
-          Parser p = Parser();
-          Expression exp = p.parse(userInput);
-          ContextModel cm = ContextModel();
-          var result = exp.evaluate(EvaluationType.REAL, cm);
-          output = result.toString();
-        } catch (e) {
-          output = "Error";
-        }
-      });
+      if (input.isNotEmpty) {
+        setState(() {
+          var userInput = input;
+          // Replace 'x' with '*' for multiplication.
+          userInput = userInput.replaceAll("x", "*");
+          try {
+            // Create a mathematical expression parser.
+            Parser p = Parser(); 
+            // Parse the user's input into a mathematical expression
+            Expression exp = p.parse(userInput);
+            // Create a context model for variable evaluation.
+            ContextModel cm = ContextModel();
+            // Evaluate the expression as a real number using the context model.
+            var result = exp.evaluate(EvaluationType.REAL, cm);
+            // Convert the result to a string and display it.
+            output = result.toString();
+          } catch (e) {
+            // Handle any parsing or evaluation errors by displaying "Error".
+            output = "Error";
+          }
+        });
+      }
     } else {
       setState(() {
         input += value;
@@ -52,63 +57,71 @@ class _CalculatorAppState extends State<CalculatorApp> {
     }
   }
 
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         children: [
-          Expanded(child: Container(
+          Expanded(
+              child: Container(
             width: double.infinity,
-              padding: EdgeInsets.all(12),
+            padding: EdgeInsets.all(12),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(input, style:  TextStyle(
-                  fontSize: 48,
-                  color: Colors.white,
-                ),
+                Text(
+                  input,
+                  style: TextStyle(
+                    fontSize: 48,
+                    color: Colors.white,
+                  ),
                 ),
                 SizedBox(
                   height: 20,
                 ),
-                Text(output, style:  TextStyle(
-                  fontSize: 34,
-                  color: Colors.white.withOpacity(0.7),
-                ),
+                Text(
+                  output,
+                  style: TextStyle(
+                    fontSize: 34,
+                    color: Colors.white.withOpacity(0.7),
+                  ),
                 ),
                 SizedBox(
                   height: 30,
                 )
               ],
             ),
-          )
-          ),
-
-
-
-
-
+          )),
           Row(
             children: [
-              button(text: "AC", buttonBGColor: Colors.white24, tcolor: Colors.orange),
-              button(text: "<", buttonBGColor: Colors.white24, tcolor: Colors.orange),
-              button(text: "",buttonBGColor: Colors.transparent),
-              button(text: "/", buttonBGColor: Colors.white24, tcolor: Colors.orange),
+              button(
+                  text: "AC",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
+              button(
+                  text: "<",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
+              button(text: "", buttonBGColor: Colors.transparent),
+              button(
+                  text: "/",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
             ],
           ),
           Row(
             children: [
               button(text: "7"),
               button(text: "8"),
-              button(text: "9",),
-              button(text: "x", buttonBGColor: Colors.white24, tcolor: Colors.orange),
+              button(
+                text: "9",
+              ),
+              button(
+                  text: "x",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
             ],
           ),
           Row(
@@ -116,7 +129,10 @@ class _CalculatorAppState extends State<CalculatorApp> {
               button(text: "4"),
               button(text: "5"),
               button(text: "6"),
-              button(text: "-", buttonBGColor: Colors.white24, tcolor: Colors.orange),
+              button(
+                  text: "-",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
             ],
           ),
           Row(
@@ -124,47 +140,51 @@ class _CalculatorAppState extends State<CalculatorApp> {
               button(text: "1"),
               button(text: "2"),
               button(text: "3"),
-              button(text: "+", buttonBGColor: Colors.white24, tcolor: Colors.orange),
+              button(
+                  text: "+",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
             ],
           ),
           Row(
             children: [
-              button(text: "%", buttonBGColor: Colors.white24, tcolor: Colors.orange),
+              button(
+                  text: "%",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
               button(text: "0"),
-              button(text: ".", buttonBGColor: Colors.white24, tcolor: Colors.orange),
+              button(
+                  text: ".",
+                  buttonBGColor: Colors.white24,
+                  tcolor: Colors.orange),
               button(text: "=", buttonBGColor: Colors.orange),
             ],
           ),
-
         ],
       ),
     );
   }
 
-  Widget button({
-    text, tcolor = Colors.white, buttonBGColor = Colors.white10
-}){
-    return Expanded(child: Container(
+  Widget button({text, tcolor = Colors.white, buttonBGColor = Colors.white10}) {
+    return Expanded(
+        child: Container(
       margin: EdgeInsets.all(8),
-      child:
-      ElevatedButton(
+      child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12)
-            ),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
             padding: const EdgeInsets.all(22),
-            primary: buttonBGColor
-        ),
+            primary: buttonBGColor),
         onPressed: () => onButtonClick(text),
-        child:  Text(text, style: TextStyle(
-          fontSize: 18,
-          color: tcolor,
-          fontWeight: FontWeight.bold,
-        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 18,
+            color: tcolor,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-    )
-    );
+    ));
   }
-
 }
